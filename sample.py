fe0061501df0c659bcb7bf5797d542b96e1efefb
ankibhat1995 <ankibhat1995@gmail.com>
2025-10-01 16:12:25 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,18 @@
-a=10 type(a) int
-b=10.5 type(b) float
-a=7+3j  type(a) complex 
+a=10 type(a) **int**
+b=10.5 type(b) **float**
+a=7+3j  type(a) **complex** 
 print(a.real)  7.0
 print(a.imag)  3.0
+name="Ankita" print(name) type(name) Ankita,str  ----**String**
+my_tuple = (10, 20, 30, 40) print(my_tuple)----- **tuple**
+fruits = ["apple", "banana", "cherry"]   **List**
+print(fruits[1])          # Accessing items
+fruits[1] = "mango"       # Changing items
+fruits.append("orange")   # Adding items at end
+fruits.insert(1, "grape") # Adding items in specific place .
+fruits.remove("mango")    # remove by value
+fruits.pop(0)             # remove by index
+del fruits[1]             # delete by index
+my_set = {1, 2, 3, 4, 4, 2}                 **set**
+{1, 2, 3, 4}   # duplicates removed automatically
+person = {"name": "Ankita","age": 30,"city": "Kolkata"}   **Dictionary**
